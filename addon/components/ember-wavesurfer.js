eb995877ab9f3760c1bf64bebbe819a2916f8254
dougbanville <doug.banville@rte.ie>
2019-02-04 12:28:39 +0000

    basic wave

--- addon/components/ember-wavesurfer.js
+++ addon/components/ember-wavesurfer.js
@@ -1,6 +1,17 @@
-import Ember from 'ember';
-import layout from '../templates/components/ember-wavesurfer';
+import Component from "@ember/component";
+import WaveSurfer from "wavesurfer.js";
 
-export default Ember.Component.extend({
-  layout
+export default Component.extend({
+  url: `https://s3-eu-west-1.amazonaws.com/audiofile-uploads/english-news/190121011059.mp3`,
+
+  didInsertElement() {
+    this._super(...arguments);
+    console.log(`HI from wavesurfer ${this.get("url")}`);
+    var wavesurfer = WaveSurfer.create({
+      container: "#waveform",
+      waveColor: "violet",
+      progressColor: "purple"
+    });
+    wavesurfer.load(this.get("url"));
+  }
 });
