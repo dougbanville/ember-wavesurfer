998e696dfb53153fb6a02240c0decceaa05b3dfe
Doug Banville <doug.banville@rte.ie>
2019-08-26 14:35:42 +0100

    added dummy audiofile and plugins

--- addon/components/ember-wavesurfer.js
+++ addon/components/ember-wavesurfer.js
@@ -1,10 +1,14 @@
 import Component from "@ember/component";
 import WaveSurfer from "wavesurfer.js";
+import TimelinePlugin from "wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js";
+import MinimapPlugin from "wavesurfer.js/dist/plugin/wavesurfer.minimap.min.js";
 import { inject as service } from "@ember/service";
+import { htmlSafe } from "@ember/string";
 
 export default Component.extend({
   wavesurferStatus: service(),
 
+  isReady: false,
   url: null,
   audioRate: 1,
   autoCenter: true,
@@ -25,40 +29,51 @@ export default Component.extend({
   loopSelection: true,
   maxCanvasWidth: 4000,
   renderer: "MultiCanvas",
-  responsive: false,
-  scrollParent: false,
+  responsive: true,
+  scrollParent: true,
   skipLength: 2,
   splitChannels: false,
   xhr: null,
+  showTimeLine: true,
 
   didInsertElement() {
     this._super(...arguments);
-    var wavesurfer = WaveSurfer.create({
-      audioRate: this.get("audioRate"),
-      autoCenter: this.get("autoCenter"),
-      barHeight: this.get("barHeight"),
-      barGap: this.get("barGap"),
+    const params = {
+      audioRate: this.audioRate,
+      autoCenter: this.autoCenter,
+      barHeight: this.barHeight,
+      barGap: this.barGap,
       container: "#waveform",
-      waveColor: this.get("waveColor"),
-      progressColor: this.get("progressColor"),
-      barWidth: this.get("barWidth"),
-      cursorColor: this.get("cursorColor"),
-      cursorWidth: this.get("cursorWidth"),
-      fillParent: this.get("fillParent"),
-      forceDecode: this.get("forceDecode"),
-      height: this.get("height"),
-      hideScrollbar: this.get("hideScrollbar"),
-      interact: this.get("interact"),
-      loopSelection: this.get("loopSelection"),
-      maxCanvasWidth: this.get("maxCanvasWidth"),
-      MultiCanvas: this.get("MultiCanvas"),
-      responsive: this.get("responsive"),
-      scrollParent: this.get("scrollParent"),
-      skipLength: this.get("skipLength"),
-      splitChannels: this.get("splitChannels"),
-      xhr: this.get("xhr")
-    });
-    wavesurfer.load(this.get("url"));
+      waveColor: this.waveColor,
+      progressColor: this.progressColor,
+      barWidth: this.barWidth,
+      cursorColor: this.cursorColor,
+      cursorWidth: this.cursorWidth,
+      fillParent: this.fillParent,
+      forceDecode: this.forceDecode,
+      height: this.height,
+      hideScrollbar: this.hideScrollbar,
+      interact: this.interact,
+      loopSelection: this.loopSelection,
+      maxCanvasWidth: this.maxCanvasWidth,
+      MultiCanvas: this.MultiCanvas,
+      responsive: this.responsive,
+      scrollParent: this.scrollParent,
+      skipLength: this.skipLength,
+      splitChannels: this.splitChannels,
+      xhr: this.xhr
+    };
+    if (this.showTimeLine) {
+      params.plugins = [
+        TimelinePlugin.create({
+          container: "#wave-timeline"
+        }),
+        MinimapPlugin.create()
+      ];
+    }
+
+    var wavesurfer = WaveSurfer.create(params);
+    wavesurfer.load(this.url);
     wavesurfer.on("error", e => {
       this.set("error", e);
     });
@@ -76,7 +91,18 @@ export default Component.extend({
       this.wavesurferStatus.playerStatus("paused");
     });
     wavesurfer.on("stop", () => {
-      this.get("wavesurferStatus").playerStatus("stopped");
+      this.wavesurferStatus.playerStatus("stopped");
+    });
+
+    wavesurfer.on("loading", percents => {
+      this.set("percentage", htmlSafe(`${percents}%`));
+      if (percents > 99) {
+        this.set("percentage", "Creating Waveform");
+      }
+    });
+
+    wavesurfer.on("ready", () => {
+      this.set("isReady", true);
     });
   },
 
